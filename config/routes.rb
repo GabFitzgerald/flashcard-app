b2604d7e7fb71db37c6caef9a731022f6dd236a6
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-05-14 11:16:14 +1000

    pages controller links working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/how_to_use'
-  get 'pages/educators'
-  get 'pages/feedback'
+  root to: "pages#home"
+  get 'pages/home', to: 'pages#home', as: 'home_page'
+  get 'pages/about', to: 'pages#about', as: 'about_page'
+  get 'pages/how_to_use', to: 'pages#how_to_use', as: 'how_to_use_page'
+  get 'pages/educators', to: 'pages#educators', as: 'educators_page'
+  get 'pages/feedback', to: 'pages#feedback', as: 'feedback_page'
   devise_for :users
   resources :users
-  root to: "users#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
